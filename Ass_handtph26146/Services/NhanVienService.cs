fe932d7fd169e538be2e9ea42420f2ac566ef1ab
HaCa18 <haduy1880@gmail.com>
2023-04-18 16:54:38 +0700

    duyha

--- Ass_handtph26146/Services/NhanVienService.cs
+++ Ass_handtph26146/Services/NhanVienService.cs
@@ -6,75 +6,76 @@ using System.Linq;
 
 namespace Ass_handtph26146.Services
 {
-	public class NhanVienService : INhanVienService
-	{
-		HoaDBContext context;
+    public class NhanVienService : INhanVienService
+    {
+        HoaDBContext context;
 
-		public NhanVienService()
-		{
-			context= new HoaDBContext();
-		}
+        public NhanVienService()
+        {
+            context = new HoaDBContext();
+        }
 
-		public bool CreateNv(NhanVien nv)
-		{
+        public bool CreateNv(NhanVien nv)
+        {
 
-			try
-			{
-				context.NhanViens.Add(nv);
-				context.SaveChanges();
-				return true;
+            try
+            {
+                nv.IdCV = Guid.Parse("c10001e7-5761-489e-ec00-08db2a824f89");
+                context.NhanViens.Add(nv);
+                context.SaveChanges();
+                return true;
 
-			}
-			catch (Exception)
-			{
+            }
+            catch (Exception)
+            {
 
-				return false;
-			}
-		}
+                return false;
+            }
+        }
 
-		public bool DeleteNv(Guid id)
-		{
-			try
-			{
-				var nv = context.NhanViens.Find(id);
-				context.NhanViens.Remove(nv);
-				context.SaveChanges();
-				return true;
+        public bool DeleteNv(Guid id)
+        {
+            try
+            {
+                var nv = context.NhanViens.Find(id);
+                context.NhanViens.Remove(nv);
+                context.SaveChanges();
+                return true;
 
-			}
-			catch (Exception)
-			{
+            }
+            catch (Exception)
+            {
 
-				return false;
-			}
-		}
+                return false;
+            }
+        }
 
-		public List<NhanVien> GetAllNv()
-		{
-			return context.NhanViens.ToList();
-		}
+        public List<NhanVien> GetAllNv()
+        {
+            return context.NhanViens.ToList();
+        }
 
-		public NhanVien GetNvByID(Guid id)
-		{
-			return context.NhanViens.FirstOrDefault(p => p.Id == id);
-		}
+        public NhanVien GetNvByID(Guid id)
+        {
+            return context.NhanViens.FirstOrDefault(p => p.Id == id);
+        }
 
-		public List<NhanVien> GetNvByName(string name)
-		{
-			return context.NhanViens.Where(p => p.Ten.Contains(name)).ToList();
-		}
+        public List<NhanVien> GetNvByName(string name)
+        {
+            return context.NhanViens.Where(p => p.Ten.Contains(name)).ToList();
+        }
 
-		public bool UpdateNv(NhanVien nv)
-		{
+        public bool UpdateNv(NhanVien nv)
+        {
             try
             {
                 var nhanvien = context.NhanViens.Find(nv.Id);
-				nhanvien.Id = nv.Id;
-				nhanvien.IdCV = nv.IdCV;
-				nhanvien.MatKhau = nv.MatKhau;
-				nhanvien.Sdt = nv.Sdt;
-				nhanvien.Ten = nv.Ten;
-				nhanvien.TrangThai = nv.TrangThai;
+                nhanvien.Id = nv.Id;
+                nhanvien.IdCV = nv.IdCV;
+                nhanvien.MatKhau = nv.MatKhau;
+                nhanvien.Sdt = nv.Sdt;
+                nhanvien.Ten = nv.Ten;
+                nhanvien.TrangThai = nv.TrangThai;
                 context.NhanViens.Update(nhanvien);
                 context.SaveChanges();
                 return true;
@@ -85,6 +86,27 @@ namespace Ass_handtph26146.Services
                 return false;
 
             }
+
+
         }
-	}
+
+        public bool LoginNV(string user, string pass)
+        {
+            try
+            {
+                foreach (var x in GetAllNv())
+                {
+                    if (x.Ten == user && x.MatKhau == pass)
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return false;
+        }
+    }
 }
